ddfc95bddd4451226cd208f38c53b9661f1ea53a
stepupanalytics <30996513+stepupanalytics@users.noreply.github.com>
2017-08-22 16:55:39 +0530

    Update titanicgit.R

--- titanicgit.R
+++ titanicgit.R
@@ -25,7 +25,6 @@ is.factor(titanic$Embarked)
 #removing missing value by Knn Approach
 library(DMwR)
 ?knnImputation
-titanic <- knnImputation(titanic[,!names(titanic) %in% "Survived"])
 titanic <- knnImputation(titanic)
 
 
